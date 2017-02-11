79d8c17d641f407909e5fc46e03f23e45d91e982
Dmitry <df.creative@gmail.com>
2017-02-11 21:54:08 +0300

    es5ify

--- index.js
+++ index.js
@@ -1,21 +1,22 @@
 'use strict'
 
-const SDF = require('tiny-sdf')
+var SDF = require('tiny-sdf')
 
 module.exports = atlas
 
 function atlas(options) {
 	options = options || {}
 
-	let canvas = options.canvas || document.createElement('canvas')
-	let family = options.family || 'monospace'
-	let shape = options.shape || [512, 512]
-	let step = options.step || [32, 32]
-	let size = options.size || 16
-	let chars = options.chars || [32, 126]
-	let bufferSize = Math.floor((step[0] - size)/2)
-	let radius = options.radius || bufferSize*1.5
-	let sdf = new SDF(size, bufferSize, radius, 0, family)
+	var canvas = options.canvas || document.createElement('canvas')
+	var family = options.family || 'monospace'
+	var shape = options.shape || [512, 512]
+	var step = options.step || [32, 32]
+	var size = options.size || 16
+	var chars = options.chars || [32, 126]
+	var bufferSize = Math.floor((step[0] - size)/2)
+	var radius = options.radius || bufferSize*1.5
+	var sdf = new SDF(size, bufferSize, radius, 0, family)
+	var i, j
 
 	if (typeof size === 'number') {
 		size = size + 'px'
@@ -29,9 +30,9 @@ function atlas(options) {
 		&& typeof chars[0] === 'number'
 		&& typeof chars[1] === 'number'
 	) {
-		let newchars = []
+		var newchars = []
 
-		for (let i = chars[0], j = 0; i <= chars[1]; i++) {
+		for (i = chars[0], j = 0; i <= chars[1]; i++) {
 			newchars[j++] = String.fromCharCode(i)
 		}
 
@@ -42,27 +43,27 @@ function atlas(options) {
 	canvas.width  = shape[0]
 	canvas.height = shape[1]
 
-	let ctx = canvas.getContext('2d')
+	var ctx = canvas.getContext('2d')
 
 	ctx.fillStyle = '#000'
 	ctx.fillRect(0, 0, canvas.width, canvas.height)
 	ctx.font = size + ' ' + family
 	ctx.textBaseline = 'middle'
 
-	let x = 0
-	let y = 0
-	let len = Math.min(chars.length, Math.floor(shape[0]/step[0]) * Math.ceil(shape[1]/step[1]))
+	var x = 0
+	var y = 0
+	var len = Math.min(chars.length, Math.floor(shape[0]/step[0]) * Math.ceil(shape[1]/step[1]))
 
 	// hack tiny-sdf to render centered
 	//FIXME: get rif of it by [possibly] PR to tiny-sdf
-	let align = sdf.ctx.textAlign
-	let buffer = sdf.buffer
+	var align = sdf.ctx.textAlign
+	var buffer = sdf.buffer
 
 	sdf.ctx.textAlign = 'center'
 	sdf.buffer = sdf.size/2
 
-	for (let i = 0; i < len; i++) {
-		let data = sdf.draw(chars[i])
+	for (i = 0; i < len; i++) {
+		var data = sdf.draw(chars[i])
 
 		ctx.putImageData(data, x, y)
 
